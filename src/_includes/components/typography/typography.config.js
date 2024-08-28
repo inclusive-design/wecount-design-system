05e6abd7acd9883393563c2812638f41cd45b4df
Ned Zimmerman <ned@bight.dev>
2024-08-28 12:11:33 -0300

    fix: update default typography string
    Signed-off-by: Ned Zimmerman <ned@bight.dev>

--- src/_includes/components/typography/typography.config.js
+++ src/_includes/components/typography/typography.config.js
@@ -2,7 +2,7 @@ module.exports = {
     title: "Typography",
     context: {
         level: "p",
-        text: "It is the responsibility of inclusive designers to be aware of the context and broader impact of any design and strive to effect a beneficial impact beyond the intended beneficiary of the design."
+        text: "We Count is a community-driven project to address the inherent bias against small minorities and outliers in artificial intelligence (AI) and data analytics. Disability often places you at the margins of a data set. More and more critical decisions are made and automated using data. Current artificial intelligence can automate and amplify existing discrimination."
     },
     variants: [
         {
